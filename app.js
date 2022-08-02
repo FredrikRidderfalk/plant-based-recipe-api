4127a97b431a697725de88aa6dc071b3f15e5755
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-08-02 10:43:20 +0200

    add connection to db

--- app.js
+++ app.js
@@ -3,26 +3,57 @@ const app = express(); // We execute the package
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
 require("dotenv/config");
 
-// Middlewares - A middlewear is a function that executes when routes are being hit
-app.use(bodyParser.json()); // A middlewear
-app.use(cors());
+// v2
 
-// Import routes
-const recipesRoute = require("./routes/recipes");
+const uri = process.env.MONGODB_URI;
 
-app.use("/recipes", recipesRoute);
+// define the first route
+app.get("/recipes", async (req, res) => {
+  const client = new MongoClient(uri);
+  const databasesList = await client.db().admin().listDatabases();
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("we are on home, habibi!");
-}); // We use 'app' to create routes. 'get' shoots us back a message. We can also use 'post', 'delete', etc.
+  try {
+    await client.connect();
 
-// Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, () =>
-  console.log("connected to DB!!")
-);
+    const allRecipes = await client
+      .db("test")
+      .collection("recipes")
+      .find()
+      .toArray();
+    console.log("Connected to the server!");
+    console.log("allRecipes", allRecipes);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
 
 //How do we start listening to the server? Like this...
 app.listen(3000);
+
+// v1
+
+// Middlewares - A middlewear is a function that executes when routes are being hit
+// app.use(bodyParser.json()); // A middlewear
+// app.use(cors());
+
+// Import routes
+// const recipesRoute = require("./routes/recipes");
+
+// app.use("/recipes", recipesRoute);
+
+// Routes
+// app.get("/", (req, res) => {
+//   res.send("we are on home, habibi!");
+// }); // We use 'app' to create routes. 'get' shoots us back a message. We can also use 'post', 'delete', etc.
+
+// Connect to DB
+// mongoose.connect(process.env.DB_CONNECTION, () =>
+//   console.log("connected to DB!!")
+// );
+
+//How do we start listening to the server? Like this...
+// app.listen(3000);
