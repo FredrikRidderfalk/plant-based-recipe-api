78ffbf2d7e1de0fe010976aa3865ea64f17ef075
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-08-03 14:50:42 +0200

    add homepage message

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express"); // We import the package
 const app = express(); // We execute the package
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
 require("dotenv/config");
 
-////////////////////////////////////////////////////////////////
-// v2.0
 const { MongoClient } = require("mongodb");
 
 const uri = process.env.DB_CONNECTION;
 
-// define the first route
-app.get("/recipe-api/recipes", async (req, res) => {
+// Define the home route
+app.get("/", (req, res) => {
+  res.send(
+    "This is the home page. The following endpoints are available to you: /meals, /desserts, and /drinks"
+  );
+}); // We use 'app' to create routes. 'get' shoots us back a message. We can also use 'post', 'delete', etc.
+
+// Define the recipes route
+app.get("/recipes", async (req, res) => {
   const client = new MongoClient(uri);
 
   try {
@@ -32,66 +34,11 @@ app.get("/recipe-api/recipes", async (req, res) => {
   }
 });
 
+// Define the desserts route
+// Define the drinks route
+
 // // Port
 const PORT = process.env.PORT || 3000;
 
-//How do we start listening to the server? Like this...
+// Starting a server
 app.listen(PORT, () => console.log(`The server is running at ${PORT}`));
-
-////////////////////////////////////////////////////////////////
-// v1.0
-
-// Middlewares - A middlewear is a function that executes when routes are being hit
-// app.use(bodyParser.json()); // A middlewear
-// app.use(cors());
-
-// // Import routes
-// const recipesRoute = require("./routes/recipes");
-
-// app.use("/recipe-api", recipesRoute);
-
-// // Routes
-// app.get("/", (req, res) => {
-//   res.send(
-//     "This is the home page. Add an endpoint like 'recipes' to start your foodie journey."
-//   );
-// }); // We use 'app' to create routes. 'get' shoots us back a message. We can also use 'post', 'delete', etc.
-
-// // Connect to DB
-// mongoose.connect(process.env.DB_CONNECTION, () =>
-//   console.log("Database connection successful.")
-// );
-
-// // // Port
-// const PORT = process.env.PORT || 3000;
-
-// // // Starting a server
-// app.listen(PORT, () => console.log(`The server is running at ${PORT}`));
-
-////////////////////////////////////////////////////////////////
-// v1.1
-
-// Middlewares - A middlewear is a function that executes when routes are being hit
-// app.use(bodyParser.json()); // A middlewear
-// app.use(cors());
-
-// // Routes
-// const recipesRoute = require("./routes/recipes");
-
-// app.use("/recipe-api", recipesRoute);
-
-// // Connect to DB
-// mongoose
-//   .connect(process.env.DB_CONNECTION, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Database connection successful.");
-//   });
-
-// // Port
-// const PORT = process.env.PORT || 3000;
-
-// // Starting a server
-// app.listen(PORT, () => console.log(`The server is running at ${PORT}`));
