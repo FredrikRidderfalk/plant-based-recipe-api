6a3a7f61da462b2d320f78a37719406640a45f1a
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-08-03 12:22:10 +0200

    .

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.DB_CONNECTION;
 
 // define the first route
-app.get("/", async (req, res) => {
+app.get("/recipe-api/recipes", async (req, res) => {
   const client = new MongoClient(uri);
 
   try {
