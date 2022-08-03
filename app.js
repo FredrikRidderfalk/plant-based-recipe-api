5e84ee3ad2860706ad6653d0b29b3865dab8ab70
Fredrik Ridderfalk <ridderfalk@gmail.com>
2022-08-03 11:51:30 +0200

    switch to v2.0

--- app.js
+++ app.js
@@ -7,62 +7,64 @@ require("dotenv/config");
 
 ////////////////////////////////////////////////////////////////
 // v2.0
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-// const uri = process.env.MONGODB_URI;
+const uri = process.env.DB_CONNECTION;
 
-// // define the first route
-// app.get("/recipes", async (req, res) => {
-//   const client = new MongoClient(uri);
+// define the first route
+app.get("/recipe-api", async (req, res) => {
+  const client = new MongoClient(uri);
 
-//   try {
-//     await client.connect();
+  try {
+    await client.connect();
 
-//     const allRecipes = await client
-//       .db("test")
-//       .collection("recipes")
-//       .find()
-//       .toArray();
-//     console.log("Connected to the server!");
-//     console.log("allRecipes", allRecipes);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// });
+    const allRecipes = await client
+      .db("test")
+      .collection("recipes")
+      .find()
+      .toArray();
+    console.log("Connected to the server!");
+    console.log("allRecipes", allRecipes);
 
-// //How do we start listening to the server? Like this...
-// app.listen(3000);
+    return res.json(allRecipes);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
+
+//How do we start listening to the server? Like this...
+app.listen(3000);
 
 ////////////////////////////////////////////////////////////////
 // v1.0
 
 // Middlewares - A middlewear is a function that executes when routes are being hit
-app.use(bodyParser.json()); // A middlewear
-app.use(cors());
+// app.use(bodyParser.json()); // A middlewear
+// app.use(cors());
 
-// Import routes
-const recipesRoute = require("./routes/recipes");
+// // Import routes
+// const recipesRoute = require("./routes/recipes");
 
-app.use("/recipe-api", recipesRoute);
+// app.use("/recipe-api", recipesRoute);
 
-// Routes
-app.get("/", (req, res) => {
-  res.send(
-    "This is the home page. Add an endpoint like 'recipes' to start your foodie journey."
-  );
-}); // We use 'app' to create routes. 'get' shoots us back a message. We can also use 'post', 'delete', etc.
+// // Routes
+// app.get("/", (req, res) => {
+//   res.send(
+//     "This is the home page. Add an endpoint like 'recipes' to start your foodie journey."
+//   );
+// }); // We use 'app' to create routes. 'get' shoots us back a message. We can also use 'post', 'delete', etc.
 
-// Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, () =>
-  console.log("Database connection successful.")
-);
+// // Connect to DB
+// mongoose.connect(process.env.DB_CONNECTION, () =>
+//   console.log("Database connection successful.")
+// );
 
-// // Port
-const PORT = process.env.PORT || 3000;
+// // // Port
+// const PORT = process.env.PORT || 3000;
 
-// // Starting a server
-app.listen(PORT, () => console.log(`The server is running at ${PORT}`));
+// // // Starting a server
+// app.listen(PORT, () => console.log(`The server is running at ${PORT}`));
 
 ////////////////////////////////////////////////////////////////
 // v1.1
